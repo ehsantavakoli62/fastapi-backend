919317ab8a7de5d94cd86c61521da162f268b1a1
Ehsan Tavakoli <ehsantavakoli62@gmail.com>
2025-12-10 21:16:21 +0300

    Update router.py

--- src/api/router.py
+++ src/api/router.py
@@ -1,1 +1,14 @@
+# src/api/router.py
 
+from fastapi import APIRouter
+
+# ایجاد نمونه APIRouter
+router = APIRouter()
+
+
+@router.get("/status")
+def get_status():
+    """
+    مسیر تست سلامت برنامه
+    """
+    return {"status": "ok", "message": "API is running"}
