f560ae6c4771df4d95620a4de7dc05796eabff12
Ehsan Tavakoli <ehsantavakoli62@gmail.com>
2025-12-10 21:13:55 +0300

    Update token.py

--- src/schemas/token.py
+++ src/schemas/token.py
@@ -1,1 +1,12 @@
+# src/schemas/token.py
 
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenPayload(BaseModel):
+    user_id: int | None = None
