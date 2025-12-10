d23e7125358eb79deb9f37692f68b826a2a160a4
Ehsan Tavakoli <ehsantavakoli62@gmail.com>
2025-12-10 21:36:49 +0300

    Update router.py

--- src/api/router.py
+++ src/api/router.py
@@ -1,9 +1,14 @@
 # src/api/router.py
 
 from fastapi import APIRouter
+from . import auth
 
 # ایجاد نمونه APIRouter
-router = APIRouter()
+# تمام مسیرها تحت پیشوند /api/v1 قرار می‌گیرند
+router = APIRouter(prefix="/api/v1")
+
+# افزودن روتر احراز هویت
+router.include_router(auth.router)
 
 
 @router.get("/status")
