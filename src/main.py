10cbc7a61f74c36914c66d146858dcbef30505d6
Ehsan Tavakoli <ehsantavakoli62@gmail.com>
2025-12-10 21:15:22 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
+# src/main.py
 
+from fastapi import FastAPI
+from .api import router as api_router
+
+# ایجاد نمونه FastAPI
+app = FastAPI(
+    title="FastAPI Skillbox Project",
+    description="Backend service for user authentication and management.",
+    version="1.0.0",
+)
+
+# اضافه کردن روتر اصلی
+# روترها تمام مسیرهای API ما را شامل می‌شوند.
+app.include_router(api_router.router)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI Backend (Skillbox)"}
